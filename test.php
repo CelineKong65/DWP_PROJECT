328acff21bde44beed09ae29091a6c6d35135b94
tingxuan30 <xuanting.0330@gmail.com>
2024-06-15 00:04:31 +0800

    test done

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";  // XAMPP默认的MySQL用户名
 $password = "";  // 默认没有密码
-$dbname = "your_database_name";  // 你创建的数据库名称
+$dbname = "test";  // 你创建的数据库名称
 
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
