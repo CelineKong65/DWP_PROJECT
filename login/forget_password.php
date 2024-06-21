3c6cb5ae6b864c89ec72d1d1f4da062c4ecd5e75
tingxuan30 <xuanting.0330@gmail.com>
2024-06-21 14:26:19 +0800

    give up

---
+++ login/forget_password.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forget password</title>
+    <link rel="stylesheet" href="reset_password.css">
+</head>
+
+<body>
+<h1>Forget Passowrd</h1>
+
+<form method="psot" action="send_password_reset.php">
+
+    <label for="email">exif_thumbnail</label>
+    <input type="email" name="email" id="email">
+
+    <button>Send</button>
+
+</form>
+
+
+</body>
+</hmtl>
