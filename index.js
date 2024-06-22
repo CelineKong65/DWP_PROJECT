9ee30ced1e3383786da1c53400847d3f2d04413e
KONGLEECHING <1221208105@student.mmu.edu.my>
2024-06-22 23:45:37 +0800

    die

--- index.js
+++ index.js
@@ -24,29 +24,3 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-
-var slideIndex = 0;
-autoShowSlides();
-
-function autoShowSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(autoShowSlides, 2000); // Change image every 2 seconds
-}
-
-//pruss the button to login to buy
-function alertLogin() {
-    document.getElementById('popup-overlay').style.display = 'block';
-    document.getElementById('popup').style.display = 'block';
-}
-
-function closePopup() {
-    document.getElementById('popup-overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-}
