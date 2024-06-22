38b62c48bdd29e3dce39045518408813cc0bb0f7
tingxuan30 <xuanting.0330@gmail.com>
2024-06-23 00:41:39 +0800

    all done

--- Comment&rating/comment_rating.php
+++ Comment&rating/comment_rating.php
@@ -34,11 +34,13 @@ $conn->close();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
 </head>
+
+
 <body>
     
     <header>
-        <div id="back">
-            <a href="../User_homepage/user_homepage.html"><b>BACK TO HOME</b></a>
+        <div>
+        <a id="back" href="../User_homepage/user_homepage.html"><b>BACK TO HOME</b></a>
         </div>
         <div class="container">
             <h1>
