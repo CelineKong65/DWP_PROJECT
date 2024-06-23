aa23ccc52379af63ffbcd771328a2887a94e9c9f
Lim Siew Jin <1221208899@soffice.mmu.edu.my>
2024-06-23 16:27:45 +0800

    done

--- Admin_homepage/Admin_home.php
+++ Admin_homepage/Admin_home.php
@@ -60,6 +60,38 @@
                 <th>Comment</th>
             </tr>
         </thead>
+        <tbody>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "okaydb";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if ($conn->connect_error) 
+                {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $result = $conn->query("SELECT * FROM comment_rating");
+                if ($result->num_rows > 0) 
+                {
+                    $no = 1;
+                    while($row = $result->fetch_assoc()) 
+                    {
+                        echo "<tr>
+                                <td>{$no}</td>
+                                <td>{$row['username']}</td>
+                                <td>{$row['rating']}</td>
+                                <td>{$row['comment']}</td>
+                            </tr>";
+                        $no++;
+                    }
+                } 
+                $conn->close();
+            ?>
+        </tbody>
     </table>
 
     <h2>Contact Us list</h2>
@@ -72,6 +104,35 @@
                 <th>Message</th>
             </tr>
         </thead>
+        <tbody>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "okaydb";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $result = $conn->query("SELECT * FROM messages");
+                if ($result->num_rows > 0) {
+                    $no = 1;
+                    while($row = $result->fetch_assoc()) {
+                        echo "<tr>
+                                <td>{$no}</td>
+                                <td>{$row['user_name']}</td>
+                                <td>{$row['user_email']}</td>
+                                <td>{$row['user_message']}</td>
+                            </tr>";
+                        $no++;
+                    }
+                } 
+                $conn->close();
+            ?>
+        </tbody>
     </table>
 
     <script src="Admin_homepage.js"></script>
