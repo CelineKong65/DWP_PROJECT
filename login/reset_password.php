b0a1456c8c8c337312125183bf33f23fc97c829d
KONGLEECHING <1221208105@student.mmu.edu.my>
2024-07-01 17:23:13 +0800

    done

--- login/reset_password.php
+++ login/reset_password.php
@@ -82,7 +82,86 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password</title>
-    <link rel="stylesheet" href="reset_password.css">
+    <style>
+    body {
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        background-image: url(pb.resetpass.png);
+        background-repeat: no-repeat; 
+        background-size: 1550px 730px;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    .container {
+        background-color: #fff;
+        padding: 50px 50px;
+        border-radius: 20px;
+        box-shadow: 0 5px 50px rgba(0, 0, 0, 0.1);
+        width: 100%;
+        max-width: 600px;
+    }
+
+    h2 {
+        margin-bottom: 20px;
+        color: #333;
+        text-align: center;
+    }
+
+    .form-group {
+        margin-bottom: 15px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+        color: #555;
+    }
+
+    input[type="email"] {
+        width: 100%;
+        padding: 10px;
+        margin-top: 5px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        box-sizing: border-box;
+    }
+
+    button {
+        width: 100%;
+        padding: 10px;
+        background-color: #FF9B50;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+    button:hover {
+        background-color: rgb(255, 230, 183);
+    }
+
+    #back {
+        position: absolute;
+        top: 10px; 
+        left: 10px; 
+        color: #FF9B50;
+        background-color: #fff;
+        font-size: 20px;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        border: #FF9B50 solid;
+        border-radius: 10px;
+        text-decoration: none;
+        padding: 5px 5px;
+    }
+
+    </style>
 </head>
 <body>
 <header>
